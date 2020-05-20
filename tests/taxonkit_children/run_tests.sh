85d52c59a8fc7057302dd7bafe4022b1660f3e8c
Nuno Fonseca <firstname.lastname@gmail.com>
2020-05-20 09:11:09 +0100

    tests for the wrapper to taxonkit list

---
+++ tests/taxonkit_children/run_tests.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+PATH2SCRIPT=$(dirname "${BASH_SOURCE[0]}" )
+
+set -u
+source $PATH2SCRIPT/../tests_aux.sh
+
+echo "*** taxonkit_children"
+
+set +xe
+must_fail "taxonkit_children.sh"
+must_fail "taxonkit_children.sh ifile2"
+must_fail "taxonkit_children.sh ifile2 ofile "
+must_fail "taxonkit_children.sh ifile2 ofile nodb"
+
+cat <<EOF > ifile
+1136698
+1524213
+1524214
+EOF
+
+must_succeed "[ `taxonkit_children.sh ifile ofile && cat ofile |wc -l` \> 10 ]"
+
+
+
+echo Failed tests: $num_failed
+exit $num_failed
+

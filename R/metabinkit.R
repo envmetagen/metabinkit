257da5d46a5290f06c9a9ae4397e139df604f646
Nuno Fonseca <firstname.lastname@gmail.com>
2020-05-27 02:09:36 +0100

    New version 0.0.5

--- R/metabinkit.R
+++ R/metabinkit.R
@@ -18,7 +18,7 @@
 #
 # =========================================================
 
-metabinkit.version <- "0.0.4"
+metabinkit.version <- "0.0.5"
 
 ## Ensure that we are using a recent version of R
 ##
--- exe/metabinkit_shared.sh
+++ exe/metabinkit_shared.sh
@@ -19,7 +19,7 @@
 #
 # =========================================================
 # useful functions
-metabinkit_version="0.0.4"
+metabinkit_version="0.0.5"
 
 function print_metabinkit_version {
     echo "metabinkit version: $metabinkit_version"
